efe22a4e30dafff2f7087abe1addb90b8111016f
anacarlagb <ana.carlagb@gmail.com>
2020-01-12 13:40:43 -0200

    Get commit-based heuristic

--- src/main/java/inf/puc/rio/opus/composite/model/CompositeHeuristic.java
+++ src/main/java/inf/puc/rio/opus/composite/model/CompositeHeuristic.java
@@ -3,6 +3,7 @@ package inf.puc.rio.opus.composite.model;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 
 public class CompositeHeuristic {
@@ -25,7 +26,7 @@ public class CompositeHeuristic {
 				for (int j = 0; j < refactorings.size(); j++) {
 					// Get class name and package name of current refactoring
 
-					if (i != j  && isSameDeveloper(refactorings.get(i).commit, refactorings.get(j).commit)) {
+					if (i != j  && isSameDeveloper(refactorings.get(i).getCurrentCommit(), refactorings.get(j).getCurrentCommit())) {
 
 						for (CodeElement elementj : refactorings.get(j)
 								.getElements()) {
@@ -60,31 +61,30 @@ public class CompositeHeuristic {
 	
 	public List<CompositeRefactoring> getCommitBasedComposites(List<Refactoring> refactorings) {
 		
+	
+		Set<String> allCommits = getAllCommitsOfRefactorings(refactorings);
+		
 		List<HashSet<Refactoring>> composites = new ArrayList<HashSet<Refactoring>>();
 
-		for (int i = 0; i < refactorings.size(); i++) {
-			// Get class name and package name of current refactoring
-
+	
+		for(String commit: allCommits) {
+			
 			HashSet<Refactoring> composite = new HashSet<Refactoring>();
-			composite.add(refactorings.get(i));
-			for (CodeElement elementi : refactorings.get(i).getElements()) {
-
-				for (int j = 0; j < refactorings.size(); j++) {
-					// Get class name and package name of current refactoring
-
-					if (i != j) {
-
-						
-					}
-
-				}
+			
+			for (int i = 0; i < refactorings.size(); i++) {
+				
+				String refCommit = refactorings.get(i).getCurrentCommit().getCommit();
+				
+				if(commit.equals(refCommit)) {
+					composite.add(refactorings.get(i));
+				}	
 
 			}
-
+			
 			composites.add(composite);
-
+			
 		}
-
+		
 		composites = removeDuplicatedRefactoringsInComposites(composites);
 		return getComposites("commit-based", composites);
 	
@@ -92,6 +92,24 @@ public class CompositeHeuristic {
 	
 	
 	
+	private Set<String> getAllCommitsOfRefactorings(List<Refactoring> refactorings) {
+		// TODO Auto-generated method stub
+		
+		Set<String> allCommits = new HashSet<String>();
+		
+		for(Refactoring ref : refactorings) {
+			
+			String refCommit = ref.getCurrentCommit().getCommit();
+			
+			allCommits.add(refCommit);
+		}
+		
+		return allCommits;
+	}
+
+
+
+
 	private List<HashSet<Refactoring>> removeDuplicatedRefactoringsInComposites(List<HashSet<Refactoring>> composites) {
 		
 		for (int b = 0; b < composites.size(); b++) {
@@ -160,7 +178,7 @@ public class CompositeHeuristic {
 		
 	}
 	
-	public boolean isSameDeveloper(String commitRefi, commitRefj){
+	public boolean isSameDeveloper(Commit commitRefi, Commit commitRefj){
 	    return true;
 	}
 
